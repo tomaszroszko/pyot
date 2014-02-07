73793f4ede4d38abbca3c9a597d2cdb983061256
tomasz.roszko <tomaszroszko@gmail.com>
2014-02-07 11:56:44 +0100

    initial commits

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+#from distutils.core import setup
+from setuptools import setup, find_packages
+from pyot import __version__
+
+setup(name="pyot",
+      version=__version__,
+      description="Opentopic library for python",
+      license="MIT",
+      author="Tomasz Roszko",
+      author_email="tomaszroszko@gmail.com",
+      url="https://github.com/tomaszroszko/pyot",
+      packages=find_packages(),
+      keywords="opentopic library",
+      zip_safe=True)
